6dda8b94b449d6d353c86f1d945e790f39cb63fc
Danilo Steger <dandu2021@gmail.com>
2024-10-16 01:32:22 -0300

    arreglo de enlace y paso de datos

--- GuardarPostulacion.php
+++ GuardarPostulacion.php
@@ -5,15 +5,16 @@ session_start();
 extract($_POST);
 $nombreUser = $_SESSION['usuario'] ;
 $idUser = $_SESSION['idUser'];
+//falta obtener la id de publicacion
 $idPublicacion = '';
 
 date_default_timezone_set('America/Argentina/Buenos_Aires');
 $fecha = date('d/m/Y');
 
-if(isset($monto) &&isset($comentario)){
+if(isset($monto) && isset($comentario)){
 
     $postulacion = "INSERT INTO postulaciones (IdPublicacion, IdUsuarioPostulacion, FechaPostulacion, Monto, ComentarioPostulacion)
-    VALUES ('".$idPublicacion."', '".$idUser."', '".$fecha."', '".$monto."', '".$comentario."',) ";
+    VALUES ('".$idPublicacion."', '".$idUser."', '".$fecha."', '".$monto."', '".$comentario."') ";
     mysqli_query($conexion, $postulacion);
     header("Location: PaginaPrincipal.php?". session_id());
 }

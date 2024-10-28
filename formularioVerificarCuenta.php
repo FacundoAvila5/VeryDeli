c4d8cbfcde063bc0ad87a2f1723bf5672519acd6
Danilo Steger <dandu2021@gmail.com>
2024-10-28 17:16:25 -0300

    envio para verificacion

--- formularioVerificarCuenta.php
+++ formularioVerificarCuenta.php
@@ -7,11 +7,11 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-            <form id="verificar" action="GuardarPedidoValidacion.php" method="post" class="needs-validation" novalidate>
+            <form id="verificar" action="GuardarPedidoValidacion.php" method="post" class="needs-validation" enctype="multipart/form-data" novalidate>
                 
             <div class="mb-3">
-                <label for="tituloPubli" class="form-label">DNI</label>
-                <input type="text" class="form-control" id="tituloPubli" name="tituloPubli" placeholder="Ingrese su DNI" required>
+                <label for="dni" class="form-label">DNI</label>
+                <input type="text" class="form-control" id="dni" name="dni" placeholder="Ingrese su DNI" required>
                 <div class="invalid-feedback">
                     El DNI es obligatorio. 
                 </div>
@@ -20,15 +20,15 @@
             <div class="mb-3">
                 <div class="row">
                     <div class="col-md-6">
-                        <label for="provinciaorigen" class="form-label">CUIL</label>
-                        <input type="text" class="form-control" id="provinciaorigen" name="provinciaorigen" placeholder="Ingrese su CUIL" required>
+                        <label for="cuil" class="form-label">CUIL</label>
+                        <input type="text" class="form-control" id="cuil" name="cuil" placeholder="Ingrese su CUIL" required>
                         <div class="invalid-feedback">
                             El CUIL es obligatorio.
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <br><label for="Localidadorigen" class="form-label">Boleta de servicio a su nombre</label>
-                        <input type="file" class="form-control" id="Localidadorigen" name="Localidadorigen" required>  
+                        <br><label for="boleta" class="form-label">Boleta de servicio a su nombre</label>
+                        <input type="file" class="form-control" id="boleta" name="boleta" required>  
                         <div class="invalid-feedback">
                             Por favor, seleccione una boleta de pago de servicios a su nombre.
                         </div>
